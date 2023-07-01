5ca920d9106b41b62fcc7fe9870fe86029109c25
Daniel <66198246+DanyCDev@users.noreply.github.com>
2023-07-01 13:59:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path');
 var fs = require('fs')
 const app = express()
-const port = 3000
+const port = 80
 
 app.use(express.urlencoded({
     extended: true
